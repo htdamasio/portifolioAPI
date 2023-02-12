1ffc177ea888c0168edc7a3aa28f1b84ac0e0317
Henrique Tomé <henri.vini@hotmail.com>
2023-02-12 20:02:30 -0300

    Setup for vercel

--- src/server.ts
+++ src/server.ts
@@ -21,14 +21,14 @@ async function main() {
     const port = Number.parseInt(process.env.PORT) || 4000;
     const { url } = await server.listen(port)
     console.log(`🚀 Server ready at ${url}`)
-    githubDataFetch.start();
+    // githubDataFetch.start();
     // setTimeout(() => fetchDataFromGithub(), 1)
 }
 
 // Executed every day at 1AM
-const githubDataFetch = cron.schedule('0 0 1 * * *',
-    function fetchDataFromGithub() {
-        console.log('Will fetch data');
-        // fetch data from github and start another job to process this information and store in json files
-    });
+// const githubDataFetch = cron.schedule('0 0 1 * * *',
+//     function fetchDataFromGithub() {
+//         console.log('Will fetch data');
+//         // fetch data from github and start another job to process this information and store in json files
+//     });
 main();
